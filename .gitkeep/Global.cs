1fbac77b9a51afa88fca921dab0e8822d727af65
scr6ux <146682413+scr6ux@users.noreply.github.com>
2023-11-18 22:36:08 -0500

    Add files via upload

---
+++ .gitkeep/Global.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Galaxy_Swapper_v2.Workspace
+{
+    public static class Global
+    {
+        public const string Version = "1.27";
+        public const string ApiVersion = "1.20";
+        public static string Discord = "https://galaxyswapperv2.com/Discord.php";
+        public static string Website = "https://galaxyswapperv2.com";
+        public static string Download = "https://galaxyswapperv2.com/Downloads/InGame.php";
+        public static string Key = "https://galaxyswapperv2.com/Downloads/Discord.php";
+
+        public class CustomException : Exception
+        {
+            public CustomException(string message) : base(message)
+            {
+            }
+
+            public CustomException(string message, Exception innerException) : base(message, innerException)
+            {
+            }
+        }
+
+        public class FortniteDirectoryEmptyException : Exception
+        {
+            public FortniteDirectoryEmptyException(string message) : base(message)
+            {
+            }
+
+            public FortniteDirectoryEmptyException(string message, Exception innerException) : base(message, innerException)
+            {
+            }
+        }
+    }
+}
